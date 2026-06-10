dcbb037670baaaa420890e66498baf1cf932c869
90609 <906094554@qq.com>
2026-06-11 01:28:28 +0800

    feat: Remove old python backend

--- backend/api/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .requests import CrawlRequest
-from .response import CrawlResponse
-
-__all__ = ['CrawlRequest', 'CrawlResponse']
--- backend/core/__init__.py
+++
@@ -1,4 +0,0 @@
-from .config import Settings, get_settings
-from .events import startup_event, shutdown_event
-
-__all__ = ['Settings', 'get_settings', 'startup_event', 'shutdown_event']
